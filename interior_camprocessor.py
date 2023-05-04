5e84a3ca14603c600f410e94567fe7f9d844a2de
Sushma <SushmaChandra2023@u.northwestern.edu>
2023-05-04 10:56:43 -0500

    take pictures at regular intervals

---
+++ interior_camprocessor.py
@@ -0,0 +1,17 @@
+import cv2
+import time
+
+cap = cv2.VideoCapture(0)
+interval = 5 # interval in seconds
+counter = 0 # initialize the counter
+
+while True:
+    ret, frame = cap.read()
+    if ret:
+        # Save the frame as an image with incrementing file name
+        filename = f"image_{counter}.jpg"
+        cv2.imwrite(filename, frame)
+        counter += 1 # increment the counter variable
+    time.sleep(interval)
+
+cap.release()
