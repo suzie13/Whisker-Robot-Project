492f1f87bbd36eab0f530dd1facf3cf9844d80d6
Sushma <SushmaChandra2023@u.northwestern.edu>
2023-05-18 13:35:10 -0500

    marker ids

---
+++ get_marker_ids.py
@@ -0,0 +1,45 @@
+import cv2
+from cv2 import aruco
+import numpy as np
+import time
+
+class MarkSearch :
+
+    dict_aruco = aruco.Dictionary_get(aruco.DICT_4X4_50)
+    parameters = aruco.DetectorParameters_create()
+
+    def __init__(self, cameraID):
+        self.cap = cv2.VideoCapture(cameraID)
+
+    def get_markID(self):
+        #Obtain marker id list from still image
+        ret, frame = self.cap.read()
+        gray = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
+
+        corners, ids, rejectedImgPoints = aruco.detectMarkers(gray, dict_aruco, parameters=parameters)
+
+        list_ids = np.ravel(ids)
+
+        return list_ids
+
+ 
+if __name__ == "__main__" :
+    import cv2
+    from cv2 import aruco
+    import numpy as np
+    import time
+
+    dict_aruco = aruco.Dictionary_get(aruco.DICT_4X4_50)
+    parameters = aruco.DetectorParameters_create()
+
+    #parameter
+    cameraID = 0
+    cam0_mark_search = MarkSearch(cameraID)
+
+    try:
+        while True:
+            print(' ----- get_markID ----- ')
+            print(cam0_mark_search.get_markID())
+            time.sleep(0.5)
+    except KeyboardInterrupt:
+        cam0_mark_search.cap.release()
