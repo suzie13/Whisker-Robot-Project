2246d5708f7dbfcf23adce77336f41e4406575f7
Sushma <SushmaChandra2023@u.northwestern.edu>
2023-06-08 10:55:49 -0500

    Added columns direction and step which user can input

--- real_world_coordinates_cam1.py
+++ real_world_coordinates_cam1.py
@@ -1,12 +1,14 @@
-###################  Top Camera  #################
-
 import cv2
 import numpy as np
 import csv
 from datetime import datetime
 
-camera_matrix = np.array([[682.694, 0, 573.5379], [0, 675.82, 367.535], [0, 0, 1]]) 
-dist_coeffs = np.array([-0.119, 0.1449, 0.0019, -0.00689, -0.1701]) 
+# Default values for direction and x-step
+direction_default = "left"
+x_step_default = 200
+
+camera_matrix = np.array([[682.694, 0, 573.5379], [0, 675.82, 367.535], [0, 0, 1]])
+dist_coeffs = np.array([-0.119, 0.1449, 0.0019, -0.00689, -0.1701])
 
 # ArUco marker dictionary
 aruco_dict = cv2.aruco.getPredefinedDictionary(cv2.aruco.DICT_4X4_50)
@@ -22,12 +24,17 @@ def estimate_marker_pose(marker_corners):
     rvecs, tvecs, _ = cv2.aruco.estimatePoseSingleMarkers(marker_corners, marker_size, camera_matrix, dist_coeffs)
     return rvecs, tvecs
 
-cap = cv2.VideoCapture(6)
+cap = cv2.VideoCapture(0)
 
 # Open the output file for writing
 with open(output_file, mode='w', newline='') as file:
     writer = csv.writer(file)
-    writer.writerow(['Timestamp', 'Marker ID', 'Position (x)', 'Position (y)', 'Position (z)'])
+    # Updated header row to include direction and x-step columns
+    writer.writerow(['Timestamp', 'Marker ID', 'Position (x)', 'Position (y)', 'Position (z)', 'direction', 'x-step'])
+
+    # Prompt the user to input direction and x-step values
+    direction = input("Enter the direction (default: left): ") or direction_default
+    x_step = input("Enter the x-step (default: 200): ") or x_step_default
 
     while True:
         # Read frame from the camera
@@ -51,8 +58,8 @@ with open(output_file, mode='w', newline='') as file:
                     print("Position (x, y, z):", x, y, z)
                     # Get the current timestamp
                     timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-                    # Write coordinates to the file
-                    writer.writerow([timestamp, ids[i][0], x, y, z])
+                    # Write coordinates to the file along with direction and x-step values
+                    writer.writerow([timestamp, ids[i][0], x, y, z, direction, x_step])
 
             cv2.imshow("ArUco Marker Detection", frame)
 
