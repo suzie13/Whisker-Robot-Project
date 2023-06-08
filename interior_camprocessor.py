1fff0868af6e90b62ca3f2bb73e68d6bfc945a73
Sushma <SushmaChandra2023@u.northwestern.edu>
2023-06-08 07:49:26 -0500

    save name with date and time format

--- interior_camprocessor.py
+++ interior_camprocessor.py
@@ -1,6 +1,7 @@
 import cv2
 import time
 import os
+from datetime import datetime
 
 cap = cv2.VideoCapture(14)
 interval = 0.04  # interval in seconds (25 frames per second)(25Hz)
@@ -14,8 +15,8 @@ os.makedirs(output_folder, exist_ok=True)
 while True:
     ret, frame = cap.read()
     if ret:
-        # Get the current timestamp
-        timestamp = int(time.time())
+        # Get the current date and time as a string
+        timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
         
         # Save the frame as an image with the timestamp as the filename
         filename = os.path.join(output_folder, f"image_{timestamp}.jpg")
